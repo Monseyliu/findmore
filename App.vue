05512dfb0d974491f58684d04be023ca0c5f2247
axx_monsey <liu75776@gmail.com>
2021-01-13 17:24:02 +0800

    created home page

---
+++ App.vue
@@ -0,0 +1,20 @@
+<script>
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/* 引入字体图标 */
+	@import url("./style/iconfont.css");
+	/* 引入微信初始化样式 */
+	@import url("./style/base.wxss");
+</style>
---
+++ pages/home/home.vue
@@ -0,0 +1,31 @@
+<template>
+	<view>
+		home页面
+		<text class="iconfont icon-customer-service">12</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		},
+		onLoad() {
+			uni.request({
+				url: "http://service.picasso.adesk.com/v3/homepage/vertical",
+				success: res => {
+					console.log(res)
+				}
+			})
+		}
+	}
+</script>
+
+<style>
+
+</style>
